6120e6bbe856a4a47bb0664c3581ae1092e0de91
Tal Abutbul <112417918+tal6203@users.noreply.github.com>
2023-09-11 15:07:43 +0300

    Update SecurityConfig.java

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 
 @Configuration
@@ -51,8 +52,7 @@ public class SecurityConfig  {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
-
-   public void addCorsMappings(CorsRegistry registry) {
+  public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // You can specify URL patterns here
                 .allowedOrigins("https://safe-flight.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
